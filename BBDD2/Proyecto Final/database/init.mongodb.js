49010f23168d2c0886ac06eea32d0ccae9c24f1a
RaphaelNicaise <raphanicaise@gmail.com>
2025-07-02 20:23:20 +0000

    Proyecto Dockerizado

--- BBDD2/Proyecto Final/database/init.mongodb.js
+++ BBDD2/Proyecto Final/database/init.mongodb.js
@@ -1,39 +1,42 @@
 use ('Inventario');
 
-db.productos.insertOne([
+id_producto = ObjectId();
+id_proovedor = ObjectId();
+
+db.productos.insertOne(
 {
-  _id: ObjectId,
+  _id: id_producto,
   codigo: "PROD001",
   nombre: "Laptop HP",
   categoria: "Electrónicos",
   precio: 799.99,
   stockActual: 15,
   stockMinimo: 5,
-  proveedorId: ObjectId,
-  fechaUltimaActualizacion: ISODate
+  proveedorId: id_proovedor,
+  fechaUltimaActualizacion: ISODate()
 }
-]);
+);
 
-db.movimientos.insertOne([
+db.movimientos.insertOne(
 {
-  _id: ObjectId,
-  productoId: ObjectId,
+  _id: ObjectId(),
+  productoId: id_producto,
   tipo: "entrada", // "entrada" o "salida"
   cantidad: 10,
   motivo: "Compra a proveedor",
-  fecha: ISODate,
+  fecha: ISODate(),
   usuario: "admin"
 }
-]);
+);
 
 
-db.proovedores.insertOne([
+db.proovedores.insertOne(
 // Colección: proveedores
 {
-  _id: ObjectId,
+  _id: id_proovedor,
   nombre: "Distribuidora Tech",
   contacto: "Juan López",
   telefono: "+1234567890",
   email: "ventas@distritech.com",
   productosOfrecidos: ["PROD001", "PROD002"]
-}]);    
+});    
